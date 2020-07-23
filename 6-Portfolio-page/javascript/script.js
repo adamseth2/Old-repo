3cb4fd88c622fd816d1f8f410f713f9c96c39180
adamseth2 <35408139+adamseth2@users.noreply.github.com>
2020-07-22 20:04:19 -0700

    Portfolio almost done

--- 6-Portfolio-page/javascript/script.js
+++ 6-Portfolio-page/javascript/script.js
@@ -2,6 +2,7 @@ let hamburger = document.getElementById('hamburger');
 let navPopUp = document.getElementById('nav-pop-up');
 let secondLineHam = document.querySelector('.two');
 
+
 hamburger.addEventListener("click", openNavBar)
 document.getElementById('arrow-close').addEventListener("click", openNavBar)
 
@@ -19,3 +20,52 @@ function openNavBar() {
         secondLineHam.style.width = '100%'
     } 
 }
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#section-B", {
+    scrollTrigger:"#section-B",
+    start: "bottom bottom",
+    y: 20,
+    delay: 0.25,
+    autoAlpha: 1,
+    duration: 3
+});
+gsap.to("#section-B h1", {
+    scrollTrigger:"#s-B-title",
+    start: "bottom bottom",
+    y: 20,
+    delay: 0.01,
+    autoAlpha: 1,
+    duration: 3
+});
+gsap.to("#section-B img", {
+    scrollTrigger:"#section-B img",
+    start: "bottom",
+    delay: 0.01,
+    autoAlpha: 1,
+    duration: 2
+});
+gsap.to(".container-A p", {
+    scrollTrigger:".container p",
+    start: "bottom",
+    delay: 0.01,
+    autoAlpha: 1,
+    duration: 2
+});
+
+gsap.to(".container-B p", {
+    scrollTrigger:".container p",
+    start: "bottom",
+    delay: 0.01,
+    autoAlpha: 1,
+    duration: 2
+});
+
+gsap.to(".container-C h2", {
+    scrollTrigger:".container p",
+    start: "bottom",
+    delay: 0.01,
+    autoAlpha: 1,
+    duration: 2
+});
